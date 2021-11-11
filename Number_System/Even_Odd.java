84f2ee5d44725d9b238d3f3ac1264eab2be76cb3
anushkaa333 <68331130+anushkaa333@users.noreply.github.com>
2021-11-11 19:45:22 +0530

    Add files via upload
    Find even numbers till given number

--- Number_System/Even_Odd.java
+++ Number_System/Even_Odd.java
@@ -1,29 +1,70 @@
 package Number_System;
 
 import java.util.Scanner;
-
-public class Even_Odd {
+// To find even numbers till given number 
+public class Even_Odd 
+{
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Enter a number : ");
 		Scanner sc =  new Scanner(System.in);
-		int n = sc.nextInt();
-		System.out.println(isOdd(n));
+		//int n = sc.nextInt();
+		//System.out.println(isOdd(n));
+		
+		int count = sc.nextInt() ;
+		for(int i = 0; i < count ; i++)
+		{
+			if((i & 1) != 1)
+			{
+				System.out.print(i);
+				
+				if((count & 1) == 1)// if count number is even
+				{
+					if(i==count - 1)
+					{
+						System.out.print(".");
+					}
+					else
+					{
+						System.out.print(" , ");
+					}
+				}
+				else //if count is odd
+				{
+					if(i==count - 2)
+					{
+						System.out.print(".");
+					}
+					else
+					{
+						System.out.print(" , ");
+					}
+				}
+			}
+			
+		}
+
 	}
-	
-	public static boolean isOdd(int n)
+}
+	/*public static boolean isOdd(int n)
 	{
 		return (n & 1) ==1;
-	}
+	}*/
 
-}
-
+	
 
 /*
+ * 
+ * 
+ * Enter a number : 
+10
+0 , 2 , 4 , 6 , 8.
+
+
 
 Enter a number : 
-67
-true
-*/
+11
+0 , 2 , 4 , 6 , 8 , 10.
+ */
