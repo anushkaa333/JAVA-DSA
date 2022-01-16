b765b011da00f8ea094c12d6600ccbe0d5ecdcbd
anushkaa333 <68331130+anushkaa333@users.noreply.github.com>
2022-01-16 18:28:07 +0530

    Add files via upload

--- Tree/BS.java
+++ Tree/BS.java
@@ -1,6 +1,10 @@
 package Tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BS 
 {
@@ -10,32 +14,90 @@ public class BS
 	{
 		// TODO Auto-generated method stub
 		sc = new Scanner(System.in);
+		//int choice =0 ;
 		Node root = CreateTree();
 		//System.out.println(root);
+		/*while(choice!=5)
+		{
+			System.out.println("\n1.Inorder\n2.Pre\n3.Post\n4.Height\n5.No of nodes\n6.Level order using recursion\n7.Level order using queue ");
+			System.out.println("\n Enter your choice \n");        
+	        choice = sc.nextInt(); 
+	        switch(choice)
+	        {
+	        case 1 :
+	        	{
+	        		System.out.println("Inorder");
+	        		inorder(root);
+	        		break;
+	        	}
+	        case 2 :
+        		{
+        			System.out.println("Preorder");
+        			preorder(root);
+        			break;
+        		}
+	        case 3 :
+        		{
+        			System.out.println("Postorder");
+        			postorder(root);
+        			break;
+        		}
+	        case 4 :
+        		{
+        			System.out.println("Height of tree");
+        			int ht = height(root);
+        			System.out.println(ht);
+        		}
+	        case 5 :
+        		{
+        			System.out.println("no of nodes in tree");
+        			int sz = size(root);
+        			System.out.println(sz);
+        		}
+	        case 6 :
+        		{
+        			System.out.println("Level order using recursion : ");
+        			levelOrderTraversal(root, ht); // error not accepting ht value from case 4.. so need to write function 
+        		}
+        	
+	        }
+		}*/
+		System.out.println();
 		System.out.println("Inorder");
 		inorder(root);
 		
+		System.out.println();
 		System.out.println("Preorder");
 		preorder(root);
 		
+		System.out.println();
 		System.out.println("Postorder");
 		postorder(root);
 		
+		System.out.println();
 		System.out.println("Height of tree");
 		int ht = height(root);
 		System.out.println(ht);
 		
+		System.out.println();
 		System.out.println("no of nodes in tree");
 		int sz = size(root);
 		System.out.println(sz);
 		
+		//System.out.println("Level order using recursion : ");
+		//levelOrderTraversal(root, ht);
 		
-		System.out.println("Level order using recursion : ");
-		levelOrderTraversal(root, ht);
-		
+		System.out.println();
 		System.out.println("Level order using queue : ");
 		PrintLevelOrder(root);
+		
+		System.out.println();
+		System.out.println("Left view" );
+		LeftView(root, 0);
 
+		System.out.println();
+		System.out.println("Right view" );
+		System.out.println(RightView(root, 0));
 
 		
 	}
@@ -69,7 +131,7 @@ public class BS
 			return;
 		}
 		inorder(root.left);
-		System.out.println(root.data + " ");
+		System.out.print(root.data + " ");
 		inorder(root.right);
 	}
 	
@@ -79,7 +141,7 @@ public class BS
 		{
 			return;
 		}
-		System.out.println(root.data + " ");
+		System.out.print(root.data + " ");
 		preorder(root.left);
 		preorder(root.right);
 	}
@@ -92,7 +154,7 @@ public class BS
 		}
 		postorder(root.left);
 		postorder(root.right);
-		System.out.println(root.data + " ");
+		System.out.print(root.data + " ");
 	}
 	
 	public static int height(Node root)
@@ -131,7 +193,7 @@ public class BS
 		}
 		if(level == 1)
 		{
-			System.out.println(root.data);
+			System.out.print(root.data + " ");
 		}
 		else
 		{
@@ -161,7 +223,65 @@ public class BS
 				
 		}
 	}
+	
+	public static void LeftView(Node root, int level)
+	{
+		Queue<Node> q = new LinkedList<Node>();
+		Stack<Node> st = new Stack<Node>();
+		
+		ArrayList<Node> al = new  ArrayList<>();
+		LV(root, level, al);
+		
+		
+	}
+	
+	public static void LV(Node root, int level, ArrayList<Node> al)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		
+		if(al.size() == level)
+		{
+			al.add(root);
+			System.out.println(root.data);
+		}
+		
+		LV(root.left, level+1, al);
+		LV(root.right, level+1, al);
+	
+	}
 
+	
+	public static ArrayList<Node> RightView(Node root, int level)
+	{
+		//Queue<Node> q = new LinkedList<Node>();
+		//Stack<Node> st = new Stack<Node>();
+		
+		ArrayList<Node> al = new  ArrayList<>();
+		RV(root, level, al);
+		return al;
+		
+	}
+	
+	public static void RV(Node root, int level, ArrayList<Node> al)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		
+		if(al.size() == level)
+		{
+			al.add(root);
+			System.out.println(root.data);
+		}
+		LV(root.right, level+1, al);
+		LV(root.left, level+1, al);
+		
+	
+	}
 }
 
 
@@ -178,74 +298,13 @@ class Node
 }
 
 /*
- * Enter data : 
-2
-Enter left for : 2
-Enter data : 
-4
-Enter left for : 4
-Enter data : 
-7
-Enter left for : 7
-Enter data : 
-6
-Enter left for : 6
-Enter data : 
--1
-Enter right for : 6
-Enter data : 
--1
-Enter right for : 7
-Enter data : 
--1
-Enter right for : 4
-Enter data : 
-10
-Enter left for : 10
-Enter data : 
--1
-Enter right for : 10
-Enter data : 
--1
-Enter right for : 2
-Enter data : 
-4
-Enter left for : 4
-Enter data : 
-1
-Enter left for : 1
-Enter data : 
--1
-Enter right for : 1
-Enter data : 
--1
-Enter right for : 4
-Enter data : 
--1
-
-
-6 
-7 
-4 
-10 
-2 
-9 
-
-2 
-4 
-7 
-6 
-10 
-9 
-
-6 
-7 
-4 
-10 
-9 
-2 
-
-
+ * 							2
+						  /   \
+						 4      8
+						/ \    / \
+					   7  10   1  9
+					  /
+					 6
 Enter data : 
 2
 Enter left for : 2
@@ -289,17 +348,41 @@ Enter data :
 -1
 Enter right for : 8
 Enter data : 
+9
+Enter left for : 9
+Enter data : 
 -1
-6 7 4 10 2 1 8 Exception in thread "main" 
-2 4 7 6 10 8 1 
-6 7 10 4 1 8 2 
+Enter right for : 9
+Enter data : 
+-1
+
+Inorder
+6 7 4 10 2 1 8 9 
+Preorder
+2 4 7 6 10 8 1 9 
+Postorder
+6 7 10 4 1 9 8 2 
+Height of tree
+4
+
+no of nodes in tree
+8
+
+Level order using queue : 
+null Tree.Node@7cca494b Tree.Node@7ba4f24f null 
+
+Left view
+2
 4
 7
+6
 
-2 4 8 7 10 1 java.lang.NullPointerException: Cannot read field "data" because "root" is null
-	at Tree.BS.LOT(BS.java:126)
-	at Tree.BS.LOT(BS.java:133)
-	at Tree.BS.LOT(BS.java:133)
-	at Tree.BS.levelOrderTraversal(BS.java:114)
-	at Tree.BS.main(BS.java:30)
+
+Right view
+2
+8
+1
+6
+[Tree.Node@58372a00, Tree.Node@7cca494b, Tree.Node@4dd8dc3, Tree.Node@6d03e736]
+				
 */
